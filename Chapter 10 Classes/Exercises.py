f9871b36b317808362dee7a979212ddca1c2b63b
farid <faridnazarov@gmail.com>
2023-02-23 06:42:59 +0100

    some additional exercises from book added

--- Chapter 10 Classes/Exercises.py
+++ Chapter 10 Classes/Exercises.py
@@ -3,6 +3,7 @@ class Int_set(object):
     Same Set Implementation as Python Set
     '''
     def __init__(self):
+        self.u = None
         self._vals=[]
     def insert(self,e):
         if e not in self._vals:
@@ -16,9 +17,25 @@ class Int_set(object):
             raise ValueError(str(e), ' is not in self')
     def get_member(self):
         return self._vals[:]
+
+    def union(self, other):
+        u=other()
+        for i in other:
+            if i not in self._vals:
+                u+=i
+        return u
+    def add(self,new_elems):
+        '''new_elems is a list'''
+        self._vals+=new_elems
+    def __add__(self,other):
+        new_set=Int_set()
+        new_set._vals = self._vals + other._vals
+        return new_set
+    def __hash__(self):
+        return id(self)
     def __str__(self):
         if self._vals==[]:
-            return '{}'
+            return {}
         self._vals.sort()
         result=''
         for e in self._vals:
